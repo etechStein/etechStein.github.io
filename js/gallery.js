07c877a149f967c9f0f20337ae476cc97743185a
EvilTechnologies <105087195+etechStein@users.noreply.github.com>
2023-09-18 00:41:38 +0000

    finish js for gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,12 @@
 function activateGallery () {
-    let thumbnails =document.querySelector("#gallery-thumbs").querySelectorAll("img");
+    let thumbnails = document.querySelector("#gallery-thumbs > div > img");
 
     let mainImage = document.querySelector("#gallery-photo img");
+
+    //update img info
+    let galleryInfo = document.querySelector("#gallery-info");
+    let title = galleryInfo.querySelector(".title");
+    let description = galleryInfo.querySelector(".description");
 
     thumbnails.forEach(function(thumbnail) {
         thumbnail.addEventListener("click", function () {
@@ -9,6 +14,14 @@ function activateGallery () {
         let newImageAlt = thumbnail.dataset.description;
         mainImage.setAttribute("src", newImageSrc);
         mainImage.setAttribute("alt", newImageAlt);
+
+        //Current Image
+        document.querySelector(".current").classList.remove("current");
+        thumbnail.parentNode.classList.add("current");
+
+        //info update
+        title.innerHTML = thumbnail.dataset.title;
+        description.innerHTML = thumbnail.dataset.description;
         })
     })
 
